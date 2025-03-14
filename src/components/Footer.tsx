529466ec33ee6d64a8b2e261d856f7fc77a36b37
Vưu Trường Nhật Thanh <vuutruongnhatthanh@gmail.com>
2025-03-14 07:59:42 +0700

    update link Snake Chain in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,11 @@ const Footer: React.FC = () => {
     <footer className="mt-10 w-full text-black py-4 text-center">
       <div className="max-w-[800px] mx-auto flex flex-col items-center">
         <p className="text-sm dark:text-white">
-          © {new Date().getFullYear()} Snake Chain. All rights reserved.
+          © {new Date().getFullYear()}{" "}
+          <a href="https://snakechain.io.vn" target="_blank">
+            Snake Chain
+          </a>
+          . All rights reserved.
         </p>
       </div>
     </footer>
